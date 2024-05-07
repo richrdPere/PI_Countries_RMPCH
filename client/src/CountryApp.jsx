f6d77ebb10617dc9665c4e09b0f037d3b075d83b
Pereira Chinchero Richard Mikhael <practicanteTIC2@egemsa.com.pe>
2024-05-06 19:48:10 -0500

    2do commit del PROYECTO INTEGRADOR desde la CHM

---
+++ client/src/CountryApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+    
+
+    return (
+        <>
+            <p className='titulo'>HOLA MUNDO</p>
+        </>
+    )
+}
+
+export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import CountryApp from './CountryApp.jsx'
+import './css/index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CountryApp />
   </React.StrictMode>,
 )
